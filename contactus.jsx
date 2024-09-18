68a6de3e1328b18615b006553b4684ed5d7ddfd7
Kushan Dissanayake <kushandissanayake57@gmail.com>
2024-09-18 10:09:29 -0800

    Update contactus.jsx

--- contactus.jsx
+++ contactus.jsx
@@ -24,25 +24,25 @@ const Contact = () => {
                                 &nbsp;Assist You?
                             </span>
                         </h1>
-                        <form className="space-y-4 rounded-3xl mt-9 bg-white p-10">
+                        <form className="space-y-4 rounded-3xl mt-9 bg-white p-10" id='contact-form'>
                             <div>
                                 <label className="block mb-2 text-sm" style={{ color: "rgba(107, 114, 128, 1)" }}>Full Name</label>
-                                <Input className='light' type="full name" />
+                                <Input className='light' type="text" id='contact-form-name' required/>
                             </div>
                             <div>
                                 <label className="block mb-2 text-sm" style={{ color: "rgba(107, 114, 128, 1)" }}>Email</label>
-                                <Input className='light' type="eamil" />
+                                <Input className='light' type="eamil" id='contact-form-eamil' required/>
                             </div>
                             <div>
                                 <label className="block mb-2 text-sm" style={{ color: "rgba(107, 114, 128, 1)" }}>Phone</label>
-                                <Input className='light' type="phone number" />
+                                <Input className='light' type="phone number" id='contact-form-phone' required/>
                             </div>
                             <div>
                                 <label className="block mb-2 text-sm" style={{ color: "rgba(107, 114, 128, 1)" }}>Message</label>
-                                <Input className="light" type="massage" />
+                                <Input className="light" type="massage" id='contact-form-message' required />
                             </div>
                             <div className="text-center">
-                                <Button color="primary" radius='full' className='w-48'>
+                                <Button color="primary" radius='full' className='w-48' type='submit'>
                                     Submit
                                 </Button>
                             </div>
@@ -51,7 +51,7 @@ const Contact = () => {
                 </div>
             </div>
             {/* Footer Section */}
-            <div className="bg-white w-full flex flex-col items-center lg:items-start justify">
+            <div className="bg-white w-full flex flex-col items-center lg:items-start justify py-12">
                 <div className="w-full max-w-7x1 lg:w-5/6 xl:w-4/6 mt-10 px-5 sm:px-10 lg:px-20 2xl:px-24">
                     <h1 className="text-3xl font-bold  mb-4 text-black">We'd Love To Hear From You.</h1>
                     <div>
@@ -60,15 +60,17 @@ const Contact = () => {
                         </p>
                     </div>
                 </div>
-                <div className="w-full lg:w-7/8 xl:w-7/8 mt-6 px-5 sm:px-10 lg:px-20 2xl:px-24 py-9">
+                <div className="w-full lg:w-7/8 xl:w-7/8 mt-6 px-5 sm:px-10 lg:px-20 2xl:px-24 ">
                     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-14 text-black">
                         <div className="flex flex-col">
+                            <a href='tel:0110987000'>
                             <Button size='lg' radius='full' isIconOnly variant='flat' color="primary" className='mb-4'>
                                 <IoCallOutline size={20}/>
                             </Button>
+                            </a>
                             <h1 className="font-bold mb-2">Call Us Directly</h1>
                             <p className='text-gray-500 mb-6'>Available during working hours</p>
-                            <a class="mt-2 font-medium text-blue-600 hover:underline">011 0987 000</a>
+                            <a href="tel:0110987000"class="mt-2 font-medium text-blue-600 hover:underline">011 0987 000</a>
                         </div>
                         <div className="flex flex-col">
                             <Button size='lg' radius='full' isIconOnly variant='flat' color="primary" className='mb-4'>
@@ -79,20 +81,24 @@ const Contact = () => {
                             <a class="mt-2 font-medium text-blue-600 hover:underline">Colombo, Sri Lanka</a>
                         </div>
                         <div className="flex flex-col">
+                        <a href="mailto:contact@technook.lk?subject=Can you help me">
                             <Button size='lg' radius='full' isIconOnly variant='flat' color="primary" className='mb-4'>
                                 <MdOutlineEmail size={20}/>
                             </Button>
+                            </a>
                             <h1 className="font-bold mb-2">Email Support</h1>
                             <p className='text-gray-500 mb-6'>Our team can response in real time</p>
-                            <a class="mt-2 font-medium text-blue-600 hover:underline">contact@technook.lk</a>
+                            <a href="mailto:contact@technook.lk?subject=Can you help me"class="mt-2 font-medium text-blue-600 hover:underline">contact@technook.lk</a>
                         </div>
                         <div className="flex flex-col">
+                        <a href="mailto:contact@technook.lk?subject=Can you help me">
                             <Button size='lg' radius='full' isIconOnly variant='flat' color="primary" className='mb-4'>
                                 <Ri24HoursLine size={20}/>
                             </Button>
+                        </a>
                             <h1 className="font-bold mb-2">24/7 Support</h1>
                             <p className='text-gray-500 mb-6'>Our team can response in real time</p>
-                            <a class="mt-2 font-medium text-blue-600 hover:underline">contact@technook.lk</a>
+                            <a href="mailto:contact@technook.lk?subject=Can you help me" class="mt-2 font-medium text-blue-600 hover:underline">contact@technook.lk</a>
                         </div>
                     </div>
                 </div>
@@ -101,16 +107,3 @@ const Contact = () => {
     );
 }
 export default Contact;
-
-
-
-
-
-
-
-
-
-
-
-
-
